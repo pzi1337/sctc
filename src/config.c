02145ff33e8ae73db44ce0e30f1bf3313eef3aad
Christian Eichler <code@christian-eichler.de>
2015-05-21 14:31:54 +0200

    Implemented additional options `cache_path` and `cache_limit`
    TODO: these options are recognized by config.c, but not used in any way by
    the other parts of SCTC

--- src/config.c
+++ src/config.c
@@ -24,30 +24,49 @@
 
 #include "config.h"
 #include "helper.h"
+#include "log.h"
 
 #define SCTC_CONFIG_FILE "sctc.conf"
 
-#define OPTION_SUBSCRIBE "subscribe"
+#define OPTION_SUBSCRIBE   "subscribe"
+#define OPTION_CACHE_PATH  "cache_path"
+#define OPTION_CACHE_LIMIT "cache_limit"
 
 static char** config_subscribe = NULL;
 static int config_subscribe_count = 0;
 
+static char* cache_path;
+static int   cache_limit;
+
 void config_init() {
 	cfg_opt_t opts[] = {
-		CFG_STR_LIST(OPTION_SUBSCRIBE, "{}", CFGF_NONE),
+		CFG_STR_LIST(OPTION_SUBSCRIBE, "{}", CFGF_NONE), // the list of subscribed users
+		CFG_SIMPLE_STR(OPTION_CACHE_PATH,  &cache_path),
+		CFG_SIMPLE_INT(OPTION_CACHE_LIMIT, &cache_limit),
 		CFG_END()
 	};
+
+	/* set default values for options */
+	cache_path  = lstrdup("./cache/"); // default subdir 'cache' in bin
+	cache_limit = -1;                  // default: no limit
 
 	cfg_t *cfg = cfg_init(opts, CFGF_NOCASE);
 	cfg_parse(cfg, SCTC_CONFIG_FILE);
 
 	config_subscribe_count = cfg_size(cfg, OPTION_SUBSCRIBE);
-	config_subscribe = lcalloc(config_subscribe_count, sizeof(char*));
+	config_subscribe       = lcalloc(config_subscribe_count, sizeof(char*));
 	for(int i = 0; i < config_subscribe_count; i++) {
 		config_subscribe[i] = lstrdup(cfg_getnstr(cfg, OPTION_SUBSCRIBE, i));
 	}
 
 	cfg_free(cfg);
+
+	_log("config initialized, have values:");
+	_log("| subscriptions:");
+	for(int i = 0; i < config_subscribe_count; i++) {
+		_log("| * %s", config_subscribe[i]);
+	}
+	_log("| cache: `%s`, limit: %i", cache_path, cache_limit);
 }
 
 int config_get_subscribe_count() {
